94d28142da65880b95b85ea0ebb451b52cc9e89b
maisarahRafiq <maisarah_20000779@utp.edu.my>
2024-07-26 01:45:03 +0800

    Update servo_parallel.py

--- servo_parallel.py
+++ servo_parallel.py
@@ -1,61 +1,61 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 
 # Set up GPIO mode
 GPIO.setmode(GPIO.BOARD)
 
 # Define servo pins
-servo1_pin = 13     # GPIO 27
-servo2_pin = 15     # GPIO 22
-servo3_pin = 16     # GPIO 23
-servo4_pin = 18     # GPIO 24
+servo_pins = {
+    'servo1': 13,  # GPIO 27
+    'servo2': 15,  # GPIO 22
+    'servo3': 16,  # GPIO 23
+    'servo4': 18   # GPIO 24
+}
 
-# Set up GPIO pins
-GPIO.setup(servo1_pin, GPIO.OUT)
-GPIO.setup(servo2_pin, GPIO.OUT)
-GPIO.setup(servo3_pin, GPIO.OUT)
-GPIO.setup(servo4_pin, GPIO.OUT)
-
-# Create PWM objects for each servo
-servo1 = GPIO.PWM(servo1_pin, 50)  # GPIO 27
-servo2 = GPIO.PWM(servo2_pin, 50)  # GPIO 22
-servo3 = GPIO.PWM(servo3_pin, 50)  # GPIO 23
-servo4 = GPIO.PWM(servo4_pin, 50)  # GPIO 24
-
-# Start PWM
-servo1.start(0)
-servo2.start(0)
-servo3.start(0)
-servo4.start(0)
+# Set up GPIO pins and create PWM objects
+servos = {}
+for name, pin in servo_pins.items():
+    GPIO.setup(pin, GPIO.OUT)
+    servos[name] = GPIO.PWM(pin, 50)
+    servos[name].start(0)
 
 def set_angle(servo, angle):
     duty = angle / 18 + 2
     servo.ChangeDutyCycle(duty)
     time.sleep(0.3)
 
+def move_servo(servo_name, angles):
+    for angle in angles:
+        set_angle(servos[servo_name], angle)
+
 try:
     while True:
-        # Example: Move all servos from 0 to 180 degrees
-        for angle in range(0, 181, 10):
-            set_angle(servo1, angle)
-            set_angle(servo2, angle)
-            set_angle(servo3, angle)
-            set_angle(servo4, angle)
+        # Move from 0 to 180 degrees
+        threads = []
+        for servo_name in servos:
+            thread = threading.Thread(target=move_servo, args=(servo_name, range(0, 181, 10)))
+            threads.append(thread)
+            thread.start()
         
-        # Move back from 180 to 0 degrees
-        for angle in range(180, -1, -10):
-            set_angle(servo1, angle)
-            set_angle(servo2, angle)
-            set_angle(servo3, angle)
-            set_angle(servo4, angle)
+        for thread in threads:
+            thread.join()
+        
+        # Move from 180 to 0 degrees
+        threads = []
+        for servo_name in servos:
+            thread = threading.Thread(target=move_servo, args=(servo_name, range(180, -1, -10)))
+            threads.append(thread)
+            thread.start()
+        
+        for thread in threads:
+            thread.join()
 
 except KeyboardInterrupt:
     print("Program stopped by user")
 
 finally:
     # Clean up
-    servo1.stop()
-    servo2.stop()
-    servo3.stop()
-    servo4.stop()
+    for servo in servos.values():
+        servo.stop()
     GPIO.cleanup()
